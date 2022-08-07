42a6e404b29504099e8f088ef3557e7ba43dde9b
Imad Abdou <igentle.appletec@gmail.com>
2022-08-07 21:35:51 +0100

    fix bulk edit feature

--- service-worker.js
+++ service-worker.js
@@ -65,40 +65,43 @@ function pageLoaded(tabId) {
             let idf_group = cur_list.page.add_custom_button_group("IDF")
     
             // Bulk Fields Edit
-            cur_list.page.add_custom_menu_item(idf_group, "Bulk Rows Edit", ()=>{
+            cur_list.page.add_custom_menu_item(idf_group, "Bulk Edit", ()=>{
                 let checked = cur_list.get_checked_items();
+
                 if (checked.length == 0) {
                     frappe.show_alert("IDF: Please Select at least one row", 5);
                     return undefined;
                 }
                 let dialog_fields = []
     
+                // for (let i = 0; i < 38; i++) {
                 for (let i = 0; i < cur_list.meta.fields.length; i++) {
+
                     let field = cur_list.meta.fields[i];
-    
+                    if(field.fieldtype==="Table") continue;
+
                     dialog_fields.push({
                         label: field.label,
                         fieldname: field.fieldname,
                         fieldtype: field.fieldtype,
+                        reqd: field.reqd,
                         options: field.options
                     })
                 }
+
+                console.log(dialog_fields);
     
                 let d = new frappe.ui.Dialog({
                     title: `IDF: Bulk Edit for ${checked.length} Rows.`,
                     fields: dialog_fields,
                     primary_action_label: "Submit",
                     primary_action: function(values) {
-    
-                        for (let i = 0; i < checked.length; i++) {
-                            let checked_row = checked[i];
-                            console.log(values);
-    
+                        for (let i = checked.length - 1; i >= 0; i--) {    
                             frappe.call({
                                 method: "frappe.client.set_value",
                                 args: {
                                     doctype: cur_list.doctype,
-                                    name: checked_row.name,
+                                    name: checked[i].name,
                                     fieldname: values
                                 },
                                 callback: function(r) {
@@ -106,6 +109,8 @@ function pageLoaded(tabId) {
                                 }
                             })
                         }
+
+                        d.hide()
     
                     }
                 })
